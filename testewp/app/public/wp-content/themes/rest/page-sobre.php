932860109dbfec5dcd5b19b00152f0a308c241d4
Saulo Santos <saulo.macedoprojetos@gmail.com>
2020-05-12 16:44:53 -0300

    Add advanced custom fields

--- testewp/app/public/wp-content/themes/rest/page-sobre.php
+++ testewp/app/public/wp-content/themes/rest/page-sobre.php
@@ -7,13 +7,12 @@
 			<h2 class="subtitulo"><?php the_title(); ?></h2>
 
 			<div class="grid-8">
-				<img src="img/rest-fachada.jpg" alt="Fachada do Rest">
+				<img src="<?php the_field('foto_rest'); ?>" alt="<?php the_field('foto_rest_descricao'); ?>">
 			</div>
 
 			<div class="grid-8">
-				<h2>História</h2>
-				<p>O incentivo ao avanço tecnológico, assim como a mobilidade dos capitais internacionais auxilia a preparação e a composição dos níveis de motivação departamental.</p>
-				<p>Gostaria de enfatizar que o desenvolvimento contínuo de distintas formas de atuação prepara-nos para enfrentar situações atípicas decorrentes do remanejamento dos quadros funcionais.</p>
+				<h2><?php the_field('titulo_historia'); ?></h2>
+				<?php the_field('texto_da_historia'); ?>
 				<h2>Visão</h2>
 				<p>Não obstante, a expansão dos mercados mundiais faz parte de um processo de gerenciamento de alternativas às soluções ortodoxas.</p>
 				<h2>Valores</h2>

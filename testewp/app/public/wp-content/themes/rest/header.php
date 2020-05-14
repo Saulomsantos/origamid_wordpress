b98864294df83fc9ca50462a35692363e5935caa
Saulo Santos <saulo.macedoprojetos@gmail.com>
2020-05-14 09:06:56 -0300

    Update page title SEO

--- testewp/app/public/wp-content/themes/rest/header.php
+++ testewp/app/public/wp-content/themes/rest/header.php
@@ -2,7 +2,7 @@
 <html lang="pt-br">
 	<head>
 		<meta charset="utf-8">
-		<title><?php bloginfo('name'); ?> | <?php wp_title(''); ?> <?php the_field('title_seo'); ?> </title>
+		<title><?php bloginfo('name'); ?> | <?php the_field('title_seo'); ?> </title>
 		<meta name="description" content="<?php bloginfo('name'); ?> - <?php wp_title(''); ?> <?php the_field('description_seo'); ?>">
 
 		<link href='https://fonts.googleapis.com/css?family=Alegreya+SC' rel='stylesheet' type='text/css'>

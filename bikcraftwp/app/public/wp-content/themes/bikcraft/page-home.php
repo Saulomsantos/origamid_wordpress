0f880bf5f50905a7e85616f941b9c46bb1a69eef
Saulo <saulo.macedoprojetos@gmail.com>
2020-05-17 22:43:23 -0300

    Add include on portfolio and qualidade sections

--- bikcraftwp/app/public/wp-content/themes/bikcraft/page-home.php
+++ bikcraftwp/app/public/wp-content/themes/bikcraft/page-home.php
@@ -7,12 +7,12 @@
 
 		<section class="introducao">
 			<div class="container">
-				<h1>Bicicletas Feitas a Mão</h1>
+				<h1><?php the_field('titulo_introducao'); ?></h1>
 				<blockquote class="quote-externo">
-					<p>“não tenha nada em sua casa que você não considere útil ou acredita ser bonito”</p>
-					<cite>WILLIAM MORRIS</cite>
+					<p><?php the_field('quote_introducao'); ?></p>
+					<cite><?php the_field('citacao_introducao'); ?></cite>
 				</blockquote>
-				<a href="produtos.html" class="btn">Orçamento</a>
+				<a href="/produtos/" class="btn">Orçamento</a>
 			</div>
 		</section>
 		
@@ -22,7 +22,7 @@
 
 				<li class="grid-1-3">
 					<div class="produtos_icone">
-						<img src="img/produtos/passeio.png" alt="Bikcraft Passeio">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/produtos/passeio.png" alt="Bikcraft Passeio">
 					</div>
 					<h3>Passeio</h3>
 					<p>Muito melhor do que passear pela orla a vidros fechados.</p>
@@ -30,7 +30,7 @@
 
 				<li class="grid-1-3">
 					<div class="produtos_icone">
-						<img src="img/produtos/esporte.png" alt="Bikcraft Esporte">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/produtos/esporte.png" alt="Bikcraft Esporte">
 					</div>
 					<h3>Esporte</h3>
 					<p>Mais rápida do que Forrest Gump, ninguém vai pegar você.</p>
@@ -38,7 +38,7 @@
 
 				<li class="grid-1-3">
 					<div class="produtos_icone">
-						<img src="img/produtos/retro.png" alt="Bikcraft Retrô">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/produtos/retro.png" alt="Bikcraft Retrô">
 					</div>
 					<h3>Retrô</h3>
 					<p>O passado volta para lembrarmos o que devemos fazer no futuro.</p>
@@ -47,8 +47,8 @@
 			</ul>
 
 			<div class="call">
-				<p>clique aqui e veja os detalhes dos produtos</p>
-				<a href="produtos.html" class="btn btn-preto">Produtos</a>
+				<p><?php the_field('chamada_produtos'); ?></p>
+				<a href="/produtos/" class="btn btn-preto">Produtos</a>
 			</div>
 
 		</section>
@@ -58,39 +58,16 @@
 			<div class="container">
 				<h2 class="subtitulo">Portfólio</h2>
 				<div class="portfolio_lista">
-					<div class="grid-8"><img src="img/portfolio/retro.jpg" alt="Bicicleta Retrô"></div>
-					<div class="grid-8"><img src="img/portfolio/passeio.jpg" alt="Bicicleta Passeio"></div>
-					<div class="grid-16"><img src="img/portfolio/esporte.jpg" alt="Bicicleta Esporte"></div>
+					<?php include(TEMPLATEPATH . "/inc/clientes-portfolio.php"); ?>
 				</div>
 				<div class="call">
-					<p>conheça mais o nosso portfólio</p>
-					<a href="portfolio.html" class="btn">Portfólio</a>
+					<p><?php the_field('chamada_portfolio'); ?></p>
+					<a href="/portfolio/" class="btn">Portfólio</a>
 				</div>
 			</div>
 		</section>
 
-		<section class="qualidade container">
-			<h2 class="subtitulo">Qualidade</h2>
-			<img src="img/bikcraft-qualidade.png" alt="Bikcraft">
-			<ul class="qualidade_lista">
-				<li class="grid-1-3">
-					<h3>Durabilidade</h3>
-					<p>Sólida como pedra, leve como o vento e resistente como o diamante, são nossos diferenciais.</p>
-				</li>
-				<li class="grid-1-3">
-					<h3>Design</h3>
-					<p>Feitas sob medida para o melhor conforto e eficiência. Adaptamos a sua Bikcraft para o seu corpo.</p>
-				</li>
-				<li class="grid-1-3">
-					<h3>Sustentabilidade</h3>
-					<p>Além de ajudar a cuidar do meio ambiente, tirando carros da rua, toda a produção é sustentável.</p>
-				</li>
-			</ul>
-			<div class="call">
-				<p>conheça mais a nossa história</p>
-				<a href="sobre.html" class="btn btn-preto">Sobre</a>
-			</div>
-		</section>
+		<?php include(TEMPLATEPATH . "/inc/qualidade.php"); ?>
 
 <?php endwhile; else : endif; ?>
 
--- bikcraftwp/app/public/wp-content/themes/bikcraft/page-portfolio.php
+++ bikcraftwp/app/public/wp-content/themes/bikcraft/page-portfolio.php
@@ -34,21 +34,7 @@
 
 		<section class="portfolio">
 			<div class="container">
-				<ul class="portfolio_lista rslides_portfolio">
-
-					<li>
-						<div class="grid-8"><img src="img/portfolio/retro.jpg" alt="Bicicleta Retrô"></div>
-						<div class="grid-8"><img src="img/portfolio/passeio.jpg" alt="Bicicleta Passeio"></div>
-						<div class="grid-16"><img src="img/portfolio/esporte.jpg" alt="Bicicleta Esporte"></div>
-					</li>
-
-					<li>
-						<div class="grid-8"><img src="img/portfolio/passeio.jpg" alt="Bicicleta Passeio"></div>
-						<div class="grid-8"><img src="img/portfolio/retro.jpg" alt="Bicicleta Retrô"></div>
-						<div class="grid-16"><img src="img/portfolio/esporte.jpg" alt="Bicicleta Esporte"></div>
-					</li>
-
-				</ul>
+				<?php include(TEMPLATEPATH . "/inc/clientes-portfolio.php"); ?>
 			</div>
 		</section>
 

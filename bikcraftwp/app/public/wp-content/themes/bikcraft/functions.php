b175f4a452ad2ed684be3763d99281c44ba8f300
Saulo <saulo.macedoprojetos@gmail.com>
2020-05-25 22:50:27 -0300

    Add custom post type

--- bikcraftwp/app/public/wp-content/themes/bikcraft/functions.php
+++ bikcraftwp/app/public/wp-content/themes/bikcraft/functions.php
@@ -15,10 +15,45 @@ remove_action('admin_print_styles', 'print_emoji_styles');
 // Habilitar Menus
 add_theme_support('menus');
 
+// Custom Images Sizes
 function my_custom_sizes() {
     add_image_size('large', 1400, 380, true);
     add_image_size('medium', 768, 380, true);
 }
 add_action('after_setup_theme', 'my_custom_sizes');
+
+// Custom Post Type
+function custom_post_type_produtos() {
+	register_post_type('produtos', array(
+		'label' => 'Produtos',
+		'description' => 'Produtos',
+		'public' => true,
+		'show_ui' => true,
+		'show_in_menu' => true,
+		'capability_type' => 'post',
+		'map_meta_cap' => true,
+		'hierarchical' => false,
+		'rewrite' => array('slug' => 'produtos', 'with_front' => true),
+		'query_var' => true,
+		'supports' => array('title', 'editor', 'page-attributes','post-formats'),
+
+		'labels' => array (
+			'name' => 'Produtos',
+			'singular_name' => 'Produto',
+			'menu_name' => 'Produtos',
+			'add_new' => 'Adicionar Novo',
+			'add_new_item' => 'Adicionar Novo Produto',
+			'edit' => 'Editar',
+			'edit_item' => 'Editar Produto',
+			'new_item' => 'Novo Produto',
+			'view' => 'Ver Produto',
+			'view_item' => 'Ver Produto',
+			'search_items' => 'Procurar Produtos',
+			'not_found' => 'Nenhum Produto Encontrado',
+			'not_found_in_trash' => 'Nenhum Produto Encontrado no Lixo',
+		)
+	));
+}
+add_action('init', 'custom_post_type_produtos');
 
 ?>
